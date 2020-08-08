d8f1d82baacdbbd96f3248c519a514d86984cad2
vinaydoddapaneni <vinaydoddapaneni@gmail.com>
2020-08-08 09:58:04 +0530

    broken into small component and added styles

--- src/components/menu/menu-item.js
+++ src/components/menu/menu-item.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
-const Menuitem = ({ title, imageUrl,size }) => {
+const Menuitem = ({ title, imageUrl, size }) => {
     const styleImg = {
         backgroundImage: `url(${imageUrl})`
     }
     return (
-        <div style={styleImg} className={`${size} menu-item`}>
+        <div className={`${size} menu-item`}>
+            <div className="background-image" style={styleImg} />
             <div className="content">
-                <h1 className="title">{title}</h1>
+                <h1 className="title">{title.toUpperCase()}</h1>
                 <span className="sub-title">Shop Now</span>
             </div>
         </div>
